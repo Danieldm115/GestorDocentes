bc3cd33c9b422e944f7ff99436090b46c5561b0e
Daniel <danieldm53@educastur.es>
2025-11-24 10:22:13 +0100

    creacion del proyecto

---
+++ GestionDocentes/GestionDocentes/src/test/java/com/danieldm53/GestionDocentes/GestionDocentesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.danieldm53.GestionDocentes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionDocentesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
